9d43a7ca567f64691b63f0c6ee442745daa87140
USER <USER@DESKTOP-8LRHPT1>
2023-10-08 22:20:10 +0400

    Решение загадки

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -29,19 +29,34 @@ namespace Laba4
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(371, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Впиши в текстбокс слово \"Водопровод\"";
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(354, 222);
+            this.ClientSize = new System.Drawing.Size(416, 198);
+            this.Controls.Add(this.label1);
             this.Name = "Form3";
             this.Text = "Form3";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
