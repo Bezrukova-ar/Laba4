6dc8c5943789359dca6eb02545300c092a7811f7
USER <USER@DESKTOP-8LRHPT1>
2023-10-08 22:02:51 +0400

    Открытие дочерней формы

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace Laba4
         {
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -51,11 +52,23 @@ namespace Laba4
             this.textBox1.Size = new System.Drawing.Size(166, 29);
             this.textBox1.TabIndex = 1;
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(59, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Новая форма";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(288, 272);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -69,6 +82,7 @@ namespace Laba4
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
